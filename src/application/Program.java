4febb28352b179a3a109ba64f9f85b62e0404c28
Valdinei Tinoco <valdinei_ct@hotmail.com>
2024-10-21 22:14:19 -0300

    Sobreposicao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import entities.Account;
+import entities.SavingsAccount;
 
 public class Program {
 
@@ -9,6 +10,12 @@ public class Program {
 		Account acc1 = new Account(1001,"Alex",1000.0);
 		acc1.withdraw(200.0);
 		System.out.println(acc1.getBalance());
+		
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
+		
+		
 		
 	}
 
